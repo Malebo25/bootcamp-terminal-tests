642e1111af3e2331c2d9ba3576db8ac546807aef
malebo25 <malebomotlhake@gmail.com>
2023-07-24 13:02:38 +0200

    initial commit

---
+++ findItemsOver20.js
@@ -0,0 +1,14 @@
+export default function findItemsOver20(itemList) {
+  if (typeof itemList == "object") {
+    let results = [];
+    for (var i = 0; i < itemList.length; i++) {
+      var item = itemList[i];
+      if (item.qty > 20) {
+        results.push(item);
+      }
+    }
+    return results;
+  } else {
+    return "Input should be an object";
+  }
+}
---
+++ regCheck.js
@@ -0,0 +1,9 @@
+export default function regCheck(regNum, regEnd) {
+  if (regNum == "") {
+    return "not a valid input";
+  } else if (regNum.endsWith(regEnd)) {
+    return true;
+  } else if (!regNum.endsWith(regEnd)) {
+    return false;
+  }
+}
